4102b80e106d1a96ebb8863b1d314da8abbc333f
Greg Reyes <gregory.a.reyes@gmail.com>
2020-05-12 09:29:06 -0400

    feat: adding scrollToTopIcon and updating README file

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 ## Description
 
-A DOM manipulation exercise using vanilla JavaScript demonstrating how to dynamically build a navigation menu, and adding different types of events to manipulate and change the state of the DOM based on the user's activity.
+A DOM manipulation exercise using vanilla JavaScript demonstrating how to dynamically build a navigation menu, and adding different types of events to manipulate and alter the state of the DOM based on the user's activity.
 
 ## Techonologies
 
--- js/app.js
+++ js/app.js
@@ -14,76 +14,88 @@
 */
 
 document.addEventListener( 'DOMContentLoaded', function() {
-    // Start - Define Global Variables
-    const navbarUl = document.getElementById( 'navbar__list' );
-    const sections = document.querySelectorAll( 'section' );
-    const activeClassName = 'your-active-class';
-    // End - Define Global Variables
+  // Start - Define Global Variables
+  const navbarUl = document.getElementById( 'navbar__list' );
+  const sections = document.querySelectorAll( 'section' );
+  // End - Define Global Variables
 
-    // Start Helper Functions
-    function buildNavBasedOnAmountOfSections( children = null, parent = null ) {
-      if ( children === null || parent === null ) {
-        return 'please pass both the children and parent parameters' +
-          'to the buildNavBasedOnAmountOfSections() function';
-      } else {
-        const navbarMenu = document.getElementsByClassName( 'navbar__menu' )[0];
-        navbarMenu.style.cssText = 'background: #000; padding: 2em;';
-        const fragment = document.createDocumentFragment();
+  // Start Helper Functions
+  function buildNavBasedOnAmountOfSections( children = null, parent = null ) {
+    if ( children === null || parent === null ) {
+      return 'please pass both the children and parent parameters' +
+        'to the buildNavBasedOnAmountOfSections() function';
+    } else {
+      const navbarMenu = document.getElementsByClassName( 'navbar__menu' )[0];
+      navbarMenu.style.cssText = 'background: #000; padding: 2em;';
 
-        for ( const child of children ) {
-          child.querySelectorAll( 'h2' )[0].setAttribute( 'style', 'margin-top: 1.8em;')
-          const newElement = document.createElement( 'li' );
+      const fragment = document.createDocumentFragment();
 
-          newElement.style.cssText = 'padding-left: 1em;'
-          newElement.innerText = child.getAttribute( 'data-nav' );
-          fragment.appendChild( newElement );
-        }
+      for ( const child of children ) {
+        child.querySelectorAll( 'h2' )[0].setAttribute( 'style', 'margin-top: 1.8em;')
+        const newElement = document.createElement( 'li' );
 
-        parent.appendChild( fragment );
+        newElement.style.cssText = 'padding-left: 1em;'
+        newElement.innerText = child.getAttribute( 'data-nav' );
+        fragment.appendChild( newElement );
       }
+
+      parent.appendChild( fragment );
+    }
+  }
+  // End Helper Functions
+
+  // start building the nav
+  buildNavBasedOnAmountOfSections( sections, navbarUl );
+  // end building the nav
+
+  // Begin Events
+  navbarUl.addEventListener( 'click', e => {
+    e.preventDefault();
+    const dataNavSection = document.querySelector( `[data-nav="${e.target.innerText}"]`);
+
+    // Scroll to section on link click
+    const sectionID = document.getElementById( dataNavSection.id );
+    sectionID.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+  });
+
+  // Start - add scroll to top functionality
+  const mainSection = document.querySelector( 'main' );
+  mainSection.insertAdjacentHTML( 'beforeend', '<div class="scroll-to-top"><i class="fa fa-arrow-up"></i></div>' );
+
+  const scrollToTopIconWrap = document.getElementsByClassName( 'scroll-to-top' )[0];
+  scrollToTopIconWrap.style.cssText = 'display:none;';
+
+  const scrollToTopIcon = document.getElementsByClassName( 'fa-arrow-up' )[0];
+  scrollToTopIcon.style.cssText = 'padding: 0.7em';
+
+  scrollToTopIcon.addEventListener( 'click', e => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  // end - add scroll to top functionality
+
+  // Add class 'active' to section when near top of viewport
+  const activeClassName = 'your-active-class';
+
+  document.addEventListener( 'scroll', function() {
+
+    if ( mainSection.getBoundingClientRect().top < -220 ) {
+      scrollToTopIconWrap.style.cssText = 'display: block; margin: 0 2em; position: fixed; bottom: 1em;' +
+        ' right: 0; border-radius: 50%; background-color: rgba(0, 0, 0, 0.35);';
+    } else {
+      scrollToTopIconWrap.style.cssText = 'display:none;';
     }
 
-    function addActiveClassToSection( activeSection = null, sections = null ) {
-      if ( activeSection === null || sections === null ) {
-        return 'addActiveClassToSection requires activeSection and sections parameters';
+    for ( const section of sections ) {
+      if ( section.getBoundingClientRect().top < 120 && section.getBoundingClientRect().bottom > 200  ) {
+        section.classList.add( activeClassName );
       } else {
-        for ( const section of sections ) {
-          section.id === activeSection.id ? section.classList.add( activeClassName ) : section.classList.remove( activeClassName );
+        if ( section.className === activeClassName ) {
+          section.classList.remove( activeClassName );
         }
       }
     }
-    // End Helper Functions
-
-    // start building the nav
-    buildNavBasedOnAmountOfSections( sections, navbarUl );
-    // end building the nav
-
-    // Begin Events
-    navbarUl.addEventListener( 'click', e => {
-      e.preventDefault();
-      const dataNavSection = document.querySelector( `[data-nav="${e.target.innerText}"]`);
-
-      // Scroll to section on link click
-      const sectionID = document.getElementById( dataNavSection.id );
-      sectionID.scrollIntoView( { behavior: 'smooth', block: 'start' } );
-
-      // Add class 'active' to section when near top of viewport
-      addActiveClassToSection( sectionID, sections );
-    });
-    // End Events
-
-    document.addEventListener( 'scroll', function() {
-      for ( const section of sections ) {
-        console.log( section.id + ' section.getBoundingClientRect().top ----> ', section.getBoundingClientRect().top );
-        if ( section.getBoundingClientRect().top < 120 && section.getBoundingClientRect().bottom > 200  ) {
-          section.classList.add( activeClassName );
-        } else {
-          if ( section.className === activeClassName ) {
-            section.classList.remove( activeClassName );
-          }
-        }
-      }
-    });
+  });
+  // End Events
 
 });
 
